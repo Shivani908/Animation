d87ef9e10bc9332579473146306af0c4576327ca
Shivani90 <jyotishukla05274@gmail.com>
2024-12-22 19:04:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const target = document.getElementById('target');
+let yes = document.getElementById('yes');
+const answer = document.getElementById('answer')
+
+yes.addEventListener('click', () => {
+  alert("yes i know you");
+});
+
+
+function moveTarget(){
+    const maxWith = 500;
+    const maxHeight = 500;
+
+    const ranomX = Math.floor(Math.random() * maxWith);
+    const ranomY = Math.floor(Math.random() * maxHeight);
+
+    target.style.left = ranomX + 'px';
+    target.style.top = ranomY + 'px'
+}
+
+
+target.addEventListener('mouseenter',function(){
+    moveTarget();
+})
