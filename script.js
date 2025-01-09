d387a5ab81f12e2a902f7a9e10046385e5244632
Shivani90 <jyotishukla05274@gmail.com>
2025-01-09 21:08:02 +0530

    lkjh

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ let yes = document.getElementById('yes');
 var ans = document.getElementById("ans")
 
 function showans(){
-document.getElementById("ans").innerHTML = "<h1> Yes I Know You </h1>"
-// document.getElementById("yes").onclick = showans;
+document.getElementById("ans").innerHTML = "<h1> I Love You Too...😘 </h1>"
 }
 document.getElementById("yes").onclick = showans;
 
 
 
 function moveTarget(){
-    const maxWith = 500;
-    const maxHeight = 500;
+    const maxWith = 300;
+    const maxHeight = 300;
 
     const ranomX = Math.floor(Math.random() * maxWith);
     const ranomY = Math.floor(Math.random() * maxHeight);
